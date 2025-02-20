73369c2eee4c1757766162680791dc83654941c4
ep2k2 <ross.a.allen@gmail.com>
2025-02-20 09:59:17 +0000

    removed page and sync for lAI

--- writing-practice/app/init_streamlit_app.py
+++ writing-practice/app/init_streamlit_app.py
@@ -6,6 +6,5 @@ app_dir = os.path.dirname(os.path.abspath(__file__))
 print("App directory:", app_dir)  # Debug info
 
 pg = st.navigation([st.Page(page="000_Learn_Kana.py", url_path='Learn_Kana'),
-                    st.Page(page="00_Romaji_to_kana.py", url_path='Romaji_to_kana'),
-                    st.Page(page="01_Kana_to_romaji.py", url_path='Kana_to_romaji')])
+                    st.Page(page="00_Romaji_to_kana.py", url_path='Romaji_to_kana')])
 pg.run()
