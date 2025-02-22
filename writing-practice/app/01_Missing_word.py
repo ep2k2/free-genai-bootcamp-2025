eb4615cf854c370bd3dc6c61bdf362c0e0e104b0
ep2k2 <ross.a.allen@gmail.com>
2025-02-22 12:37:26 +0000

    sync up

--- writing-practice/app/01_Missing_word.py
+++ writing-practice/app/01_Missing_word.py
@@ -33,8 +33,6 @@ def get_new_sentence():
             model=model,
             messages=[{"role": "user", "content": prompt}]
         )
-
-        print("API Response:", response.text)
         
         # Parse response
         parsed = json.loads(response.choices[0].message.content)
